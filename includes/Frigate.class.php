62eb6317139de107b743402687b0ba832140e66d
rankierm <raphrel@gmail.com>
2015-04-16 15:11:31 +0200

    add Ship class

---
+++ includes/Frigate.class.php
@@ -0,0 +1,22 @@
+<?php
+
+	class Frigate extends Ship
+	{
+		$this->_name			= "Hand Of The Emperor";
+
+		const	TYPE			= "Imperial_Frigate";
+		const	SIZE			= "1x4";
+		const	SPEED			= 15;
+		const	WEAPON			= array ("Naval_Spear");
+		const	PP				= 10;
+		const	MOVE			= 4
+	
+		protected $_pc			=	5;
+		protected $_pp_spend	=	0;
+		protected $_moving		=	FALSE;
+		protected $_activated	=	FALSE;
+
+		
+	}
+
+?>
---
+++ includes/Ship.class.php
@@ -0,0 +1,12 @@
+<?php
+
+	class Ship
+	{
+		protected		$_name;
+		protected		$_shieldi = 0;
+		protected		$_sprite;
+		protected		$_coords;
+
+	}
+
+?>
