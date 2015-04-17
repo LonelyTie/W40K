31eedc380ea73a14328f346f524480bfcfe0d96f
ajodin <ajodin@student.42.fr>
2015-04-17 08:01:05 +0200

    tmp move

--- includes/Move.class.php
+++ includes/Move.class.php
@@ -1,33 +1,56 @@
 <?php
 include_once("Frigate.class.php");
-class Move
+trait Move
 {
-	const PIVOT = 0;
-	const MOVE = 1;
 	private $_current_mov = 0;
 	private $_speed_points;
 	private $total_dice;
 
-	public function ft_pivot($ship)
+	public function ft_pivot($dir)
 	{
-		if ($this->_current_mov >= $ship::MOVE || $ship->is_moving == FALSE)
+		if ($this->_current_mov >= MOVE || $this->is_moving == FALSE)
 		{
+			if ($dir == 1 && $this->getCoord()['dir'] == self::NORTH)
+			{
 
+			}
+			elseif ($dir == 1 && $this->getCoord()['dir'] == self::NORTH)
+			{
+
+			}
+			elseif ($dir == 1 && $this->getCoord()['dir'] == self::NORTH)
+			{
+
+			}
+			elseif ($dir == 1 && $this->getCoord()['dir'] == self::NORTH)
+			{
+
+			}
+			elseif ($dir == 2 && $this->getCoord()['dir'] == self::NORTH)
+			{
+
+			}
+			elseif ($dir == 2 && $this->getCoord()['dir'] == self::NORTH)
+			{
+
+			}
+			elseif ($dir == 2 && $this->getCoord()['dir'] == self::NORTH)
+			{
+
+			}
+			elseif ($dir == 2 && $this->getCoord()['dir'] == self::NORTH)
+			{
+
+			}
 		}
 	}
-	public function ft_move($ship, $dices)
+	public function ft_move($ship, $SpeedVal)
 	{
-		$total_dice = new Dice();
-		$total_dice->roll_the_dice($dices['move']);
-		$this->_speed_points = $ship->getSpeed() + $total_dice->merge_dice();
-		if ($ship->getMoving() == false) {
-
-		}
-		else {
-			if ($ship->prev_mov  )
-		}
+		$this->_speed_points = $this->getSpeed() + $SpeedVal;
 
 	}
+
+
 
 }
 ?>
